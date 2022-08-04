3d6e65285e93e2a6a2339086f2580fcd68f67436
EC2 Default User <ec2-user@ip-172-31-19-196.us-west-1.compute.internal>
2022-08-04 05:33:27 +0000

    7dfin

--- Gemfile
+++ Gemfile
@@ -55,6 +55,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
-gem 'jquery-rails'
+
 
 
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
     @books = Book.find(params[:id])
     @user = @books.user
     @book_comment = BookComment.new
-    
+
     @book_detail = Book.find(params[:id])
     unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
       current_user.view_counts.create(book_id: @book_detail.id)
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
       }
     @book = Book.new
     @user = current_user
-    
+
     #@book_detail = Book.find(params[:id])
     unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
       current_user.view_counts.create(book_id: @book.id)
@@ -81,6 +81,10 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body, :image, :star)
+  end
+
+  def validate_on_update
+    errors.add(:star) if star_changed?
   end
 
   def correct_user
